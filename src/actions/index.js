64cff3b82d05f2f853b3bb5462d1ae05c79195c0
Felipe Fernandes <felipedfe@gmail.com>
2022-03-31 17:28:51 -0300

    requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 // Coloque aqui suas actions
+
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+
+export const actionSaveEmail = (value) => ({
+  type: SAVE_EMAIL,
+  value,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,85 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { actionSaveEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      senha: '',
+      isDisabled: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.setState({
+        isDisabled: this.inputValidation(),
+      });
+    });
+  }
+
+  validateEmail = (email) => {
+    const regex = /\S+@\S+\.\S+/;
+    return regex.test(email);
+  }
+
+  inputValidation = () => {
+    const { senha, email } = this.state;
+    const MIN_PASSWORD_LENGTH = 6;
+    if (senha.length >= MIN_PASSWORD_LENGTH
+      && this.validateEmail(email)) {
+      return false;
+    }
+    return true;
+  }
+
+  loginWallet = () => {
+    const { sendEmail, history } = this.props;
+    const { email } = this.state;
+    sendEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, senha, isDisabled } = this.state;
+    // console.log(this.props.history);
+    return (
+      <div className="login-container">
+        <input
+          type="text"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+          data-testid="email-input"
+          placeholder="E-mail"
+        />
+        <input
+          type="text"
+          name="senha"
+          value={ senha }
+          onChange={ this.handleChange }
+          data-testid="password-input"
+          placeholder="Senha"
+        />
+        <button
+          type="button"
+          disabled={ isDisabled }
+          onClick={ this.loginWallet }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  sendEmail: (email) => dispatch(actionSaveEmail(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
