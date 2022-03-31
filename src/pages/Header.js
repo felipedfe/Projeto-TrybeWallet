2306b30bf24c28f6042b1009d97be787f6b83c19
Felipe Fernandes <felipedfe@gmail.com>
2022-03-31 18:06:58 -0300

    requisitos 2 e 3

---
+++ src/pages/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <div className="header-container">
+        <p data-testid="email-field">
+          E-mail:
+          {email}
+        </p>
+        <p data-testid="total-field">
+          Despesa Total: R$
+          {0}
+        </p>
+        <p data-testid="header-currency-field">
+          Câmbio:
+          {' '}
+          {'BRL'}
+        </p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from './Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    // const { children } = this.props;
+    return (
+      <div>
+        <Header />
+        <h1>Wallet</h1>
+      </div>
+    );
   }
 }
 
