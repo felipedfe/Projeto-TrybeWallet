6cf62ecd9c95f25521e2531337f2fbadd97cb9a4
Felipe Fernandes <felipedfe@gmail.com>
2022-04-01 16:06:22 -0300

    requisito 7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -123,6 +123,18 @@ class Wallet extends React.Component {
             </button>
           </label>
         </fieldset>
+        <h3>Tabela</h3>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
       </div>
     );
   }
